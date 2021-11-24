a486d6e7ae15bcefd9c71efc60db6b5ea33136b2
David Höppner <david@argodis.de>
2021-11-24 11:02:03 +0100

    Hardcode keyvault connection infos for testing

--- compute/app/main.py
+++ compute/app/main.py
@@ -1,13 +1,22 @@
+import sys
 
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
+from azure.core.exceptions import ServiceRequestError
 
-keyVaultName = "<your-unique-keyvault-name>"
+keyVaultName = "xffed-key-vault"
 KVUri = f"https://{keyVaultName}.vault.azure.net"
-secretName = "mySecret"
+secretName = "0xffeasaprod-connection-string"
 
 credential = DefaultAzureCredential()
+
 client = SecretClient(vault_url=KVUri, credential=credential)
-retrieved_secret = client.get_secret(secretName)
+
+try:
+    retrieved_secret = client.get_secret(secretName)
+except ServiceRequestError:
+    # log existing
+    sys.exit(1)
+
 
 print(f"The value of secret '{secretName}' in '{keyVaultName}' is: '{retrieved_secret.value}'")
